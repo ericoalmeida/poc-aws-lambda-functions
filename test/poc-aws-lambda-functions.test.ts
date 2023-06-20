e02d1fb2c80659024a959cef010ef6a39351296a
Erico Almeida <erico.silva@invillia.com>
2023-06-20 19:22:32 -0400

    feat: initial commit

---
+++ test/poc-aws-lambda-functions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PocAwsLambdaFunctions from '../lib/poc-aws-lambda-functions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/poc-aws-lambda-functions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PocAwsLambdaFunctions.PocAwsLambdaFunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
