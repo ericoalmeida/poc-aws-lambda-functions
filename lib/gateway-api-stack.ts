ebd2ebab496082f1c7894caa70bfc59d789a60a8
Erico Almeida <erico.silva@invillia.com>
2023-06-22 06:44:55 -0400

    feat: add products resource in the api gateway

--- lib/gateway-api-stack.ts
+++ lib/gateway-api-stack.ts
@@ -1,13 +1,23 @@
 import * as cdk from 'aws-cdk-lib'
 import * as awsApiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs'
 
+export interface GatewayApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambda.NodejsFunction
+}
+
 export class GatewayApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+  constructor(scope: Construct, id: string, props: GatewayApiStackProps){
     super(scope, id, props);
 
-    const apiGateway = new awsApiGateway.RestApi(this, "GatewayAPI", {
+    const api = new awsApiGateway.RestApi(this, "GatewayAPI", {
       restApiName: "GatewayAPI"
-    })
+    });
+
+    const productsFetchIntegration = new awsApiGateway.LambdaIntegration(props.productsFetchHandler);
+    
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFetchIntegration)
   }
 }
