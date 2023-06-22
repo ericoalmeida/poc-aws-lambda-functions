55784c333936f4150d0b5ec98375037ba8427259
Erico Almeida <erico.silva@invillia.com>
2023-06-22 06:50:26 -0400

    chore: add logs for api gateway

--- lib/gateway-api-stack.ts
+++ lib/gateway-api-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib'
 import * as awsApiGateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs'
 
 export interface GatewayApiStackProps extends cdk.StackProps {
@@ -11,8 +12,23 @@ export class GatewayApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: GatewayApiStackProps){
     super(scope, id, props);
 
+    const logGroup = new LogGroup(this, "GatewayAPILogs");
     const api = new awsApiGateway.RestApi(this, "GatewayAPI", {
-      restApiName: "GatewayAPI"
+      restApiName: "GatewayAPI",
+      deployOptions: {
+        accessLogDestination: new awsApiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat:awsApiGateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true
+        })
+      }
     });
 
     const productsFetchIntegration = new awsApiGateway.LambdaIntegration(props.productsFetchHandler);
