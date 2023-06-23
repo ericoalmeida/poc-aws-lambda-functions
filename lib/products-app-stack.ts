b930ddde5326f042eab0e01970000d2e81b33943
Erico Almeida <erico.silva@invillia.com>
2023-06-23 07:43:55 -0400

    chore: add dynamodb table resource

--- lib/products-app-stack.ts
+++ lib/products-app-stack.ts
@@ -1,16 +1,31 @@
 import * as awsLambda from 'aws-lambda'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 //Class to crate aws resources with cloud (stack)
 export class ProductsAppStack extends cdk.Stack {
-  //Public property that receives lambda function instance
   public readonly productsFetchHandler: lambda.NodejsFunction
+  public readonly productsDDB: dynamodb.Table
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
     super(scope, id, props);
 
+    // Creating a dynamoDB table resource
+    this.productsDDB = new dynamodb.Table(this, "ProductsDDB", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
+    
+    // Creating a lambda function resource
     this.productsFetchHandler = new lambda.NodejsFunction(this, "ProductsFetchFunction", {
       functionName: "ProductsFetchFunction",
       entry: "src/lambda/products/products-fetch-function.ts",
@@ -20,7 +35,13 @@ export class ProductsAppStack extends cdk.Stack {
       bundling: {
         minify: true,
         sourceMap: false,
+      },
+      environment: {
+        PRODUCTS_TABLE_NAME: this.productsDDB.tableName
       }
-    })
+    });
+
+    // Set permissions
+    this.productsDDB.grantReadData(this.productsFetchHandler);
   }
 }
