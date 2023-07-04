654555108d8314cafe400db668ea0739f5b6b70a
Erico Almeida <erico.silva@invillia.com>
2023-07-04 17:12:29 -0400

    feat: create product

--- src/lambda/products/create-product.function.ts
+++ src/lambda/products/create-product.function.ts
@@ -3,8 +3,14 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { Product, ProductRepository } from "/opt/nodejs/products-layer";
 
+const PRODUCT_TABLE_NAME = process.env.PRODUCT_TABLE_NAME!;
 const CREATE_PRODUCT_RESOURCE = "/product";
+
+const dbClient = new DocumentClient();
+const repository = new ProductRepository(dbClient, PRODUCT_TABLE_NAME);
 
 function checkResourceIsValid(httpMethod: string, resource: string): boolean {
   return httpMethod === "POST" && resource === CREATE_PRODUCT_RESOURCE;
@@ -14,19 +20,22 @@ export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  const { httpMethod, resource, requestContext } = event;
+  const { httpMethod, resource, requestContext, body } = event;
   const { requestId } = requestContext;
   const { awsRequestId } = context;
+
+  console.log(`RequestID: ${requestId}, Lambda RequestID: ${awsRequestId}`);
 
   const resourceIsValid = checkResourceIsValid(httpMethod, resource);
 
   if (resourceIsValid) {
-    console.log("POST: /products")
-    console.log(`RequestID: ${requestId}, Lambda RequestID: ${awsRequestId}`);
+    const requestData = JSON.parse(body!) as Product;
+
+    const product = await repository.create(requestData);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "product created" }),
+      body: JSON.stringify(product),
     };
   }
 
