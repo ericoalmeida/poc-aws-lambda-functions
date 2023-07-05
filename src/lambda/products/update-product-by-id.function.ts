b32df3a336f410a2c53c6b8050c4500f60dd70bd
Erico Almeida <erico.silva@invillia.com>
2023-07-05 07:11:17 -0400

    feat: update product

--- src/lambda/products/update-product-by-id.function.ts
+++ src/lambda/products/update-product-by-id.function.ts
@@ -3,8 +3,14 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { Product, ProductRepository } from "/opt/nodejs/products-layer";
 
+const PRODUCT_PRODUCT_NAME = process.env.PRODUCT_PRODUCT_NAME!;
 const UPDATE_PRODUCT_RESOURCES = "/product/{id}";
+
+const dbClient = new DocumentClient();
+const repository = new ProductRepository(dbClient, PRODUCT_PRODUCT_NAME);
 
 function checkResourceIsValid(httpMethod: string, resource: string): boolean {
   return httpMethod === "PUT" && resource === UPDATE_PRODUCT_RESOURCES;
@@ -14,21 +20,22 @@ export async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  const { httpMethod, resource, requestContext, pathParameters } = event;
+  const { httpMethod, resource, requestContext, pathParameters, body } = event;
   const { requestId } = requestContext;
   const { awsRequestId } = context;
+
+  console.log(`RequestID: ${requestId}, Lambda RequestID: ${awsRequestId}`);
 
   const resourceIsValid = checkResourceIsValid(httpMethod, resource);
 
   if (resourceIsValid) {
-    const productId = pathParameters!.id;
-
-    console.log(`PUT: /products/"${productId}"`);
-    console.log(`RequestID: ${requestId}, Lambda RequestID: ${awsRequestId}`);
+    const productId = pathParameters!.id!;
+    const requestData = JSON.parse(body!) as Product;
+    const product = await repository.update(productId, requestData);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Product updated" }),
+      body: JSON.stringify(product),
     };
   }
 
